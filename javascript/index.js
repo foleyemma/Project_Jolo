e4799294bebb99965e982d19e54315661aa562bd
foleyemma <follyemma03@gmail.com>
2024-03-02 21:31:55 +0100

    Changed

--- javascript/index.js
+++ javascript/index.js
@@ -1,48 +1,44 @@
-const cable = document.querySelector('.pick')
+const cable = document.querySelector(".pick");
+const connect = document.querySelector(".connect");
 
-const WALLET = document.querySelector('#pack')
+const WALLET = document.querySelector("#pack");
+const hideNav = document.querySelector("#hide");
 
+cable.addEventListener("click", function () {
+  WALLET.classList.toggle("hidden");
+});
+connect.addEventListener("click", function () {
+  WALLET.classList.toggle("hidden");
+});
+hideNav.addEventListener("click", function () {
+  WALLET.classList.toggle("hidden");
+});
 
+const pressHarvestAll = document.querySelector(".harvest");
+const confirm = document.querySelector(".holdTight");
 
-cable.addEventListener('click' , function () {
-    WALLET.classList.toggle("hidden")
-   
-})
+pressHarvestAll.addEventListener("click", function () {
+  confirm.classList.toggle("hidden");
+});
 
-const navTogglerButton = document.querySelector('.navToggler')
+const pressRubyAll = document.querySelector(".ruby");
+const cancelRubyAll = document.querySelector(".understood");
+const intact = document.querySelector(".submit");
 
-const openNavIcon = document.querySelector('.openNav')
+cancelRubyAll.addEventListener("click", function () {
+  intact.classList.toggle('hidden');
+});
 
-const closeNavIcon = document.querySelector('.closeNav')
+pressRubyAll.addEventListener("click", function () {
+  intact.classList.toggle("hidden");
+});
 
-const mainNavWidget = document.querySelector('#mainNav')
-
-
-navTogglerButton.addEventListener('click' , function () {
-    openNavIcon.classList.toggle("hidden")
-    closeNavIcon.classList.toggle("hidden")
-    mainNavWidget.classList.toggle("sm:hidden")
-})
-
-
-const pressHarvestAll = document.querySelector('.harvest')
-
-const confirm = document.querySelector('.holdTight')
-
-
-
-pressHarvestAll.addEventListener('click' , function () {
-    confirm.classList.toggle("hidden")
-   
-})
-
-const pressRubyAll = document.querySelector('.ruby')
-
-const intact = document.querySelector('.submit')
+const navToggler = document.querySelector(".navToggler");
+const smNav = document.querySelector("#smNav");
 
 
+navToggler.addEventListener("click", function () {
+  smNav.classList.toggle("hidden");
+});
 
-pressRubyAll.addEventListener('click' , function () {
-    intact.classList.toggle("hidden")
-   
-})
+
